283d344bb1734e3c229bf35ff896707f1a10ff39
henryhoo <497018787@qq.com>
2020-11-27 11:45:52 -0500

    [ui] Github banner

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,13 @@ import React, { useRef } from "react";
 import styles from "styles/Home.module.css";
 import Tree from "components/knowledgeTree";
 import { getKnowledgeTree } from "utils/knowledgeTree";
+import GithubCorner from "react-github-corner";
 
 export default function Home(props) {
   const targetRef = useRef(null);
   return (
     <div className={styles.container} ref={targetRef}>
+      <GithubCorner href="https://github.com/noobpedia/noobpedia" />
       <Tree data={props.knowledgeTreeRoot} targetRef={targetRef} />
     </div>
   );
