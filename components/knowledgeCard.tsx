b4732c0110a3f302ffb8df901e1d5528a54fd343
henryhoo <497018787@qq.com>
2020-11-27 11:52:40 -0500

    Extract tree node to a separate component

---
+++ components/knowledgeCard.tsx
@@ -0,0 +1,61 @@
+import D3Tree from "components/d3Tree";
+import React, { useEffect, useState } from "react";
+import Card from "@material-ui/core/Card";
+import { makeStyles } from "@material-ui/core/styles";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+import Divider from "@material-ui/core/Divider";
+import PlayCircleOutlineIcon from "@material-ui/icons/PlayCircleOutline";
+import { NODE_SIZE } from "components/knowledgeTree";
+
+import type { KnowledgeSubject } from "components/knowledgeTree";
+
+type TreeNodeProps = { nodeData?: KnowledgeSubject };
+
+const useStyles = makeStyles((theme) => {
+  return {
+    cardRoot: {
+      maxWidth: NODE_SIZE.width,
+      maxHeight: NODE_SIZE.height,
+      backgroundColor: theme.palette.primary.light,
+    },
+    cardContent: {
+      padding: "5px",
+    },
+    cardHeader: {
+      fontSize: "22px",
+      fontWeight: 700,
+      textTransform: "capitalize",
+    },
+    cardIcon: {
+      marginRight: "3px",
+    },
+  };
+});
+
+export default function TreeNode({
+  nodeData,
+}: TreeNodeProps): React.ReactElement {
+  const classes = useStyles();
+  const resources = nodeData.resources?.map((resource) => {
+    return (
+      <div key={resource.name + resource.link}>
+        <PlayCircleOutlineIcon className={classes.cardIcon} />
+        <a href={resource.link || "/"}>{resource.name}</a>
+      </div>
+    );
+  });
+  return (
+    <Card className={classes.cardRoot} key={nodeData.name}>
+      <CardContent className={classes.cardContent}>
+        <Typography className={classes.cardHeader} color="textPrimary">
+          {nodeData.name}
+        </Typography>
+        <Divider variant="fullWidth" />
+        <Typography variant="subtitle1" color="textPrimary">
+          {resources}
+        </Typography>
+      </CardContent>
+    </Card>
+  );
+}
--- components/knowledgeTree.tsx
+++ components/knowledgeTree.tsx
@@ -1,11 +1,6 @@
 import D3Tree from "components/d3Tree";
 import React, { useEffect, useState } from "react";
-import Card from "@material-ui/core/Card";
-import { makeStyles } from "@material-ui/core/styles";
-import CardContent from "@material-ui/core/CardContent";
-import Typography from "@material-ui/core/Typography";
-import Divider from "@material-ui/core/Divider";
-import PlayCircleOutlineIcon from "@material-ui/icons/PlayCircleOutline";
+import TreeNode from "components/knowledgeCard";
 
 enum ResourceType {
   VIDEO = "video",
@@ -24,56 +19,10 @@ export type KnowledgeSubject = {
   children: KnowledgeSubject[];
 };
 
-type TreeNodeProps = { nodeData?: KnowledgeSubject };
-
-const NODE_SIZE = { width: 300, height: 150 };
-
-const useStyles = makeStyles((theme) => {
-  return {
-    cardRoot: {
-      maxWidth: NODE_SIZE.width,
-      maxHeight: NODE_SIZE.height,
-      backgroundColor: theme.palette.primary.light,
-    },
-    cardContent: {
-      padding: "5px",
-    },
-    cardHeader: {
-      fontSize: "22px",
-      fontWeight: 700,
-      textTransform: "capitalize",
-    },
-    cardIcon: {
-      marginRight: "3px",
-    },
-  };
-});
-
-function TreeNode({ nodeData }: TreeNodeProps): React.ReactElement {
-  const classes = useStyles();
-  const resources = nodeData.resources?.map((resource) => {
-    console.log(resource.link);
-    return (
-      <div key={resource.name + resource.link}>
-        <PlayCircleOutlineIcon className={classes.cardIcon} />
-        <a href={resource.link || "/"}>{resource.name}</a>
-      </div>
-    );
-  });
-  return (
-    <Card className={classes.cardRoot} key={nodeData.name}>
-      <CardContent className={classes.cardContent}>
-        <Typography className={classes.cardHeader} color="textPrimary">
-          {nodeData.name}
-        </Typography>
-        <Divider variant="fullWidth" />
-        <Typography variant="subtitle1" color="textPrimary">
-          {resources}
-        </Typography>
-      </CardContent>
-    </Card>
-  );
-}
+export const NODE_SIZE: { width: number; height: number } = {
+  width: 300,
+  height: 150,
+};
 
 export default function Tree(props) {
   const targetRef = props.targetRef;
